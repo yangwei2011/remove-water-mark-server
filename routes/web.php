b185d28a9a7ed4b4a8c84ad18ccb5dcf037edbb3
yangwei <1195129513@qq.com>
2021-03-07 19:41:20 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('welcome');
+});
+
 Route::any('test/{action}', 'TestController@index');
